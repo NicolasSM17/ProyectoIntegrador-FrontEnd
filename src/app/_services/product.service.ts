6a93d955c9a024182b0d1481bedb66350f52fb75
NicolasSM17 <segoviamatosjesunicolas@gmail.com>
2023-11-29 22:20:13 -0500

    PRODUCCION: cambiando rutas para consumir la API en la nube

--- src/app/_services/product.service.ts
+++ src/app/_services/product.service.ts
@@ -10,53 +10,56 @@ import { Observable } from 'rxjs';
 })
 export class ProductService {
 
+  PATH_OF_API = "http://localhost:9090";
+  PATH_OF_API_PROD = "https://proyectointegrador-backend-production.up.railway.app:7528";
+
   constructor(private httpClient: HttpClient) { }
 
   public markAsDelivered(orderId){
-    return this.httpClient.get("http://localhost:9090/markOrderAsDelivered/" + orderId);
+    return this.httpClient.get(this.PATH_OF_API_PROD + "/markOrderAsDelivered/" + orderId);
   }
 
   public getAllOrderDetailsForAdmin(status: string): Observable<MyOrderDetails[]>{
-    return this.httpClient.get<MyOrderDetails[]>("http://localhost:9090/getAllOrderDetails/" + status);
+    return this.httpClient.get<MyOrderDetails[]>(this.PATH_OF_API_PROD + "/getAllOrderDetails/" + status);
   }
 
   public getMyOrders(): Observable<MyOrderDetails[]>{
-    return this.httpClient.get<MyOrderDetails[]>("http://localhost:9090/getOrderDetails");
+    return this.httpClient.get<MyOrderDetails[]>(this.PATH_OF_API_PROD + "/getOrderDetails");
   }
 
   public deleteCartItem(cartId){
-    return this.httpClient.delete("http://localhost:9090/deleteCartItem/" + cartId);
+    return this.httpClient.delete(this.PATH_OF_API_PROD + "/deleteCartItem/" + cartId);
   }
 
   public addProduct(product: FormData){
-    return this.httpClient.post<Product>("http://localhost:9090/addNewProduct", product);
+    return this.httpClient.post<Product>(this.PATH_OF_API_PROD + "/addNewProduct", product);
   }
 
   public getAllProducts(pageNumber, searchKeyword: string = ""){
-    return this.httpClient.get<Product[]>("http://localhost:9090/getAllProducts?pageNumber=" + pageNumber + "&searchKey=" + searchKeyword);
+    return this.httpClient.get<Product[]>(this.PATH_OF_API_PROD + "/getAllProducts?pageNumber=" + pageNumber + "&searchKey=" + searchKeyword);
   }
 
   public getProductDetailsById(productId){
-    return this.httpClient.get<Product>("http://localhost:9090/getProductDetailsById/" + productId);
+    return this.httpClient.get<Product>(this.PATH_OF_API_PROD + "/getProductDetailsById/" + productId);
   }
 
   public deleteProduct(productId: number){
-    return this.httpClient.delete("http://localhost:9090/deleteProductDetails/" + productId)
+    return this.httpClient.delete(this.PATH_OF_API_PROD + "/deleteProductDetails/" + productId)
   }
 
   public getProductDetails(isSingleProductCheckout, productId){
-    return this.httpClient.get<Product[]>("http://localhost:9090/getProductDetails/" + isSingleProductCheckout + "/" + productId);
+    return this.httpClient.get<Product[]>(this.PATH_OF_API_PROD + "/getProductDetails/" + isSingleProductCheckout + "/" + productId);
   }
 
   public placeOrder(orderDetails: OrderDetails, isCartCheckout){
-    return this.httpClient.post("http://localhost:9090/placeOrder/" + isCartCheckout, orderDetails)
+    return this.httpClient.post(this.PATH_OF_API_PROD + "/placeOrder/" + isCartCheckout, orderDetails)
   }
 
   public addToCart(productId){
-    return this.httpClient.get("http://localhost:9090/addToCart/" + productId);
+    return this.httpClient.get(this.PATH_OF_API_PROD + "/addToCart/" + productId);
   }
 
   public getCartDetails(){
-    return this.httpClient.get("http://localhost:9090/getCartDetails");
+    return this.httpClient.get(this.PATH_OF_API_PROD + "/getCartDetails");
   }
 }
--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -9,29 +9,30 @@ import { User } from '../_model/user.model';
 export class UserService {
 
   PATH_OF_API = "http://localhost:9090";
+  PATH_OF_API_PROD = "https://proyectointegrador-backend-production.up.railway.app:7528";
   requestHeader = new HttpHeaders( {"No-Auth":"True"} );
 
   constructor(private httpClient: HttpClient, 
               private userAuthService:UserAuthService) { }
 
   public register(registerData){
-    return this.httpClient.post(this.PATH_OF_API + '/registerNewUser', registerData);
+    return this.httpClient.post(this.PATH_OF_API_PROD + '/registerNewUser', registerData);
   }
 
   public login(loginData){
-    return this.httpClient.post(this.PATH_OF_API + "/authenticate", loginData, {headers: this.requestHeader});
+    return this.httpClient.post(this.PATH_OF_API_PROD + "/authenticate", loginData, {headers: this.requestHeader});
   }
 
   public forUser(){
-    return this.httpClient.get(this.PATH_OF_API + '/forUser', {responseType: 'text'});
+    return this.httpClient.get(this.PATH_OF_API_PROD + '/forUser', {responseType: 'text'});
   }
 
   public forAdmin(){
-    return this.httpClient.get(this.PATH_OF_API + '/forAdmin', {responseType: 'text'});
+    return this.httpClient.get(this.PATH_OF_API_PROD + '/forAdmin', {responseType: 'text'});
   }
 
   public getAllUsers(){
-    return this.httpClient.get<User[]>(this.PATH_OF_API + '/getAllUsers');
+    return this.httpClient.get<User[]>(this.PATH_OF_API_PROD + '/getAllUsers');
   }
 
   public roleMatch(allowedRoles): boolean {
